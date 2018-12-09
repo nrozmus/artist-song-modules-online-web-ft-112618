b41b7f4cd4fc3460a5ae1ca90200ec7b8dfb883a
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-12-09 03:06:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,17 @@
-require 'pry'
-
-class Song
-  attr_accessor :name
+ attr_accessor :name
   attr_reader :artist
-
-  @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
-  def self.all
+   extend Memorable
+   @@songs = []
+   def initialize
+@@ -18,14 +20,6 @@ def self.all
     @@songs
   end
-
-  def self.reset_all
+   def self.reset_all
     self.all.clear
   end
-
-  def self.count
+   def self.count
     self.all.count
   end
-
-  def artist=(artist)
+   def artist=(artist)
     @artist = artist
   end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
